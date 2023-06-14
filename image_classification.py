05800907a043084240366cd4045f590cd77f3f9c
Sam Noyes <snoyes1234@gmail.com>
2023-06-14 17:56:24 +0200

    Delete unnecessary assets

--- image_classification.py
+++ image_classification.py
@@ -17,7 +17,7 @@ urllib.request.install_opener(opener)
 # Load and preprocess the image
 config = resolve_data_config({}, model=model)
 transform = create_transform(**config)
-url, filename = ("https://a-z-animals.com/media/animals/images/original/lion7.jpg", "lion7.jpg")
+url, filename = ("https://a-z-animals.com/media/animals/images/original/lion7.jpg", "images/lion7.jpg")
 urllib.request.urlretrieve(url, filename)
 img = Image.open(filename).convert('RGB')
 tensor = transform(img).unsqueeze(0) # transform and add batch dimension
